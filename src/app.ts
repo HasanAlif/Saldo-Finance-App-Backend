7c51600a913fc6c27d44983aad303e961789e6ee
Mehedi Hasan Alif <alifalmehedihasan@gmail.com>
2026-03-28 11:19:38 +0600

    CORS FIX

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ export const corsOptions = {
   origin: [
     "http://localhost:3001",
     "http://localhost:3000",
-    "http://localhost:5173/",
+    "http://localhost:5173",
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
